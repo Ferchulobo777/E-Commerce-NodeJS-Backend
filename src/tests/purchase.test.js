c4d9e7ce87184f1b3ea1fb6274f920ea693108bc
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-06-06 18:40:10 -0300

    update

--- src/tests/purchase.test.js
+++ src/tests/purchase.test.js
@@ -2,45 +2,69 @@ const request = require('supertest');
 const app = require('../app');
 const Cart = require('../models/Cart');
 const Product = require('../models/Product');
+const Purchase = require('../models/Purchase');
 require('../models');
+
 let token;
+let product;
+let cartId;
+
 beforeAll(async () => {
-    const credentials = {
-        email:"test@user.com",
-        password:"user123"
-    }
-    const res = await request(app).post('/users/login').send(credentials);
-    token = res.body.token;
+  const credentials = {
+    email: "test@user.com",
+    password: "user123"
+  };
+  const res = await request(app).post('/users/login').send(credentials);
+  token = res.body.token;
 });
 
 test('POST /purchases should be able to checkout the cart', async () => {
-    const product = await Product.create({
-        title: "Smart Tv Led",
-        description: "televisor de alta calidad con una pantalla grande y tecnología LED. Ofrece una experiencia de visualización inmersiva con colores vibrantes y una resolución nítida. Es perfecto para disfrutar de películas, programas de televisión y juegos con una calidad de imagen impresionante.",
-        brand: "Samsung",
-        price: 299.99
-    });
+  product = await Product.create({
+    title: "Smart Tv Led",
+    description: "televisor de alta calidad con una pantalla grande y tecnología LED...",
+    brand: "Samsung",
+    price: 299.99
+  });
 
-    const cart = await Cart.create({
-        userId: 1,
-        productId: product.id,
-        quantity: 1
-    });
+  const cart = {
+    userId: 1,
+    productId: product.id,
+    quantity: 1
+  };
 
-    const res = await request(app)
-        .post('/purchases')
-        .set('Authorization', `Bearer ${token}`)
-        .send(cart);
-
-    await product.destroy();
-    expect(res.status).toBe(200);
-    expect(res.body.id).toBeDefined();
+  const res = await request(app)
+    .post('/purchases')
+    .set('Authorization', `Bearer ${token}`)
+    .send(cart);
+  const createdPurchase = res.body;
+  cartId = createdPurchase.cartId;
+  expect(res.status).toBe(200);
+  expect(createdPurchase).toBeDefined();
+  expect(createdPurchase.cartId).toBe(cartId);
 });
 
 test('GET /purchases should be able to access the user purchases', async () => {
+    await Purchase.create({
+      cartId: cartId,
+      total: 299.99,
+      quantity: 1,
+      userId: 1
+    });
+  
     const res = await request(app)
-        .get('/purchases')
-        .set('Authorization', `Bearer ${token}`)
+      .get('/purchases')
+      .set('Authorization', `Bearer ${token}`);
     expect(res.status).toBe(200);
-    expect(res.body).toHaveLength(1);
+    const userPurchases = res.body; 
+    expect(userPurchases).toHaveLength(1);
+  });
+
+afterAll(async () => {
+  if (product) {
+    await product.destroy();
+  }
+  if (cartId) {
+    await Cart.destroy({ where: { id: cartId } });
+  }
+  await Purchase.destroy({ where: { userId: 1 } }); 
 });
